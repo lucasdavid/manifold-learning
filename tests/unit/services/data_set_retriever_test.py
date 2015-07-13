bbf2945f6072258f7832a6c0d89bc84e181ba350
Lucas David <lucas.david@drexel.edu>
2015-07-12 22:02:42 -0300

    Test DataSetRetriever

---
+++ tests/unit/services/data_set_retriever_test.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+from unittest import TestCase
+
+from manifold.services import errors, DataSetRetriever
+
+
+class DataSetRetrieverTest(TestCase):
+    def test_retrieve_brainwave(self):
+        retriever = DataSetRetriever('datasets/brainwave/plrx.txt', target_column=-1)
+
+        data, target = retriever.load().split_target().retrieve()
+
+        # Assert number of samples.
+        self.assertEqual(182, len(data))
+
+        # Assert number of features.
+        self.assertEqual(12, len(data[1]))
+
+        # Assert number of samples in target array.
+        self.assertEqual(182, len(target))
+
+    def test_retrieve_nonexistent_file(self):
+        fake_file = 'nonexistentfile.data'
+
+        r = DataSetRetriever(fake_file)
+
+        with self.assertRaises(errors.RetrieverError):
+            r.load()
+
+    def test_split_target(self):
+        fake_file = 'nonexistentfile.data'
+
+        r = DataSetRetriever(fake_file)
+
+        r._data = np.array([[1, 2, 3], [4, 5, 6]])
+        data, target = r.split_target().retrieve()
+
+        self.assertTrue((data == np.array([[1, 2], [4, 5]])).all())
+        self.assertTrue((target == np.array([3, 6])).all())
+
+        r._data = np.array([[1], [4]])
+        data, target = r.split_target().retrieve()
+
+        self.assertTrue((data == np.array([])).all())
+        self.assertTrue((target == np.array([1, 4])).all())
+
+        r._data = np.array([[1, 2, 3], [4, 5, 6]])
+        data, target = r.split_target(0).retrieve()
+
+        self.assertTrue((data == np.array([[2, 3], [5, 6]])).all())
+        self.assertTrue((target == np.array([1, 4])).all())
