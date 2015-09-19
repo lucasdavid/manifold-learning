931faf79f2dff3b25bb34198931b64448f2e712a
ldavid <lucasolivdavid@gmail.com>
2015-09-19 16:18:05 -0300

    Update 3.breast_cancer      Perform GridSearch and learn over breast_cancer data set before and after learning the manifold.

--- docs/3.breast_cancer.py
+++ docs/3.breast_cancer.py
@@ -1,4 +1,7 @@
 import time
+import multiprocessing
+
+from sklearn import grid_search, svm
 
 from manifold.learning import algorithms
 from manifold.infrastructure import Retriever, Displayer
@@ -20,14 +23,39 @@ def main():
     data, diagnosis = r.split_target(0).retrieve()
     data = data.astype(float)
 
+    d = Displayer(tite=TITLE).load('Original data-set', data, diagnosis)
+
+    learn(data, diagnosis, d)
+
     start = time.time()
     reduced_data = algorithms.Isomap(data, k=4).run()
     elapsed = time.time() - start
+    d.load('Isomap (%ss)' % (elapsed / 1000), reduced_data, diagnosis)
 
-    Displayer(tite=TITLE) \
-        .load('Original data-set', data, diagnosis) \
-        .load('Isomap (%ss)' % (elapsed / 1000), reduced_data, diagnosis) \
-        .render()
+    learn(reduced_data, diagnosis, d)
+
+    d.render()
+
+
+def learn(data, target, d):
+    start = time.time()
+    print('GridSearch started at %s...' % start)
+
+    param_grid = {
+        'C': (10, 100, 1000),
+        'gamma': (0.01, 0.1, 1),
+        'kernel': ('linear',),
+    }
+
+    clf = grid_search.GridSearchCV(svm.SVC(), param_grid, n_jobs=multiprocessing.cpu_count())
+    clf.fit(data, target)
+
+    print('\tAccuracy: %.2f%%\n'
+          '\tTime elapsed: %.0fs\n'
+          '\tBest parameters: %s'
+          % (clf.best_score_, time.time() - start, clf.best_params_))
+
+    return clf
 
 
 if __name__ == '__main__':
