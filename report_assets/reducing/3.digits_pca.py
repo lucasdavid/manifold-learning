b769602a2f3d9c9aae2cedc349fa53416f6b2d7d
ldavid <lucasolivdavid@gmail.com>
2015-10-03 16:52:37 -0300

    Update reducing report-assets

---
+++ report_assets/reducing/3.digits_pca.py
@@ -0,0 +1,23 @@
+from sklearn import datasets
+
+from report_assets.base import ReductionExample
+
+
+class ReducingDigitsExample(ReductionExample):
+    title = '1. Reducing Digits'
+
+    def _run(self):
+        digits = datasets.load_digits(n_class=3)
+
+        self.data, self.target = digits.data, digits.target
+
+        # Reduce with PCA
+        self.method = 'pca'
+        self.params = {'n_components': 3}
+        self.reduce()
+
+        self.displayer.render()
+
+
+if __name__ == '__main__':
+    ReducingDigitsExample().start()
