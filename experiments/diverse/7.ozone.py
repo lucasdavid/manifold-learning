4423f5ca5b4f19c4f8c84024acab3e4508e7e359
Lucas David <lucasolivdavid@gmail.com>
2015-11-12 17:39:57 -0200

    Update ozone experiment

--- experiments/diverse/7.ozone.py
+++ experiments/diverse/7.ozone.py
@@ -1,31 +1,44 @@
-from sklearn import datasets
+import numpy as np
+from sklearn.preprocessing import Imputer
 
 from experiments.base import ReductionExample, LearningExample
 
 
-class DigitsIsomapExample(ReductionExample, LearningExample):
-    title = '6. Digits Reduced with Isomap and Learned Example'
+class OzoneExample(ReductionExample, LearningExample):
+    title = '7. Ozone Example'
     plotting = True
 
-    reduction_method = 'isomap'
+    reduction_method = 'skisomap'
     reduction_params = {'n_neighbors': 7}
 
+    data_file = '../../datasets/ozone/onehr.data'
+
+    def read_data(self):
+        self.data = np.genfromtxt(self.data_file, delimiter=',', missing_values='?', usecols=range(1, 74))
+
+        self.data = Imputer().fit_transform(self.data)
+
+        self.target = self.data[:, -1]
+        print('Approximately %.2f%% of the %i samples are ozone days.' % (
+            self.target.sum() / len(self.target), self.data.shape[0]))
+
     def _run(self):
-        digits = datasets.load_digits()
-        self.data, self.target = digits.data, digits.target
+        self.read_data()
 
         self.displayer.load(self.data[:, 1:4], self.target)
         print('Data set size: %.2fKB' % (self.data.nbytes / 1024))
 
         self.learn()
 
-        for dimensions in (10, 3, 2, 1):
+        for dimensions in (60, 40, 30, 20, 10, 5):
             self.reduction_params['n_components'] = dimensions
             self.reduce()
 
+            d = self.data
             self.data = self.reduced_data
             self.learn()
-            self.data = digits.data
+
+            self.data = d
             del self.reduced_data
 
         if self.plotting:
@@ -33,4 +46,4 @@ class DigitsIsomapExample(ReductionExample, LearningExample):
 
 
 if __name__ == '__main__':
-    DigitsIsomapExample().start()
+    OzoneExample().start()
