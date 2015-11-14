b13169071b762a8ee2510df6481d00607fc69542
ldavid <lucasolivdavid@gmail.com>
2015-11-14 21:56:04 -0200

    Update report
    Add Introduction sketch.

--- experiments/reducing/8.ozone.py
+++ experiments/reducing/8.ozone.py
@@ -1,17 +1,14 @@
 import numpy as np
 from sklearn.preprocessing import Imputer
+from experiments.base import ReductionExample
 
-from experiments.base import ReductionExample, LearningExample
 
-
-class OzoneExample(ReductionExample, LearningExample):
+class OzoneExample(ReductionExample):
     title = '7. Ozone Example'
     plotting = True
 
-    reduction_method = 'skisomap'
-    reduction_params = {'n_neighbors': 7}
-
     data_file = '../../datasets/ozone/onehr.data'
+    original_data = None
 
     def read_data(self):
         self.data = np.genfromtxt(self.data_file, delimiter=',', missing_values='?', usecols=range(1, 74))
@@ -19,6 +16,8 @@ class OzoneExample(ReductionExample, LearningExample):
         self.data = Imputer().fit_transform(self.data)
 
         self.target = self.data[:, -1]
+        self.data = self.original_data = self.data[:, :-1]
+
         print('Approximately %.2f%% of the %i samples are ozone days.' % (
             self.target.sum() / len(self.target), self.data.shape[0]))
 
@@ -28,18 +27,10 @@ class OzoneExample(ReductionExample, LearningExample):
         self.displayer.load(self.data[:, 1:4], self.target)
         print('Data set size: %.2fKB' % (self.data.nbytes / 1024))
 
-        self.learn()
-
-        for dimensions in (60, 40, 30, 20, 10, 5):
+        for dimensions in (3, 2,):
             self.reduction_params['n_components'] = dimensions
+            self.data = self.original_data
             self.reduce()
-
-            d = self.data
-            self.data = self.reduced_data
-            self.learn()
-
-            self.data = d
-            del self.reduced_data
 
         if self.plotting:
             self.displayer.render()
